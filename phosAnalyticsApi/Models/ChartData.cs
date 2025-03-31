dd206aad54f8fe75ee74159c81a3a9be10966c3f
WaG711 <jeusan21@mail.ru>
2025-03-31 18:34:21 +0300

    description chartdata

--- phosAnalyticsApi/Models/ChartData.cs
+++ phosAnalyticsApi/Models/ChartData.cs
@@ -5,6 +5,7 @@
         public Guid ChartDataId { get; set; }
         public string Title { get; set; }
         public Guid CategoryId { get; set; }
+        public string Description { get; set; }
 
         public List<ChartPoint> Points { get; set; } = [];
     }
